5f1c8ad352a0934af43882378d9e1f526c17ed29
gatere <mercynjerigatere@gmail.com>
2024-08-06 13:04:05 +0300

    updates

--- app.py
+++ app.py
@@ -1,3 +1,26 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy_serializer import SerializerMixin
+from flask_migrate import Migrate
+from datetime import datetime
+import pytz
+
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///yourdatabase.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+# Define East African Time timezone
+EAT = pytz.timezone('Africa/Nairobi')
+
+def get_eat_now():
+    return datetime.now(EAT)
+
+from models import User, Course, CourseContent, Payment, Enrollment, Review, Message, Accolade
+
+if __name__ == '__main__':
+    app.run(debug=True)
